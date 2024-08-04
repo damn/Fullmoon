4345d7e42988e9baf2aaca303971ad47670c6ace
Michael Sappler <michaelsappler@gmail.com>
2024-08-04 09:35:11 +0530

    Remove utils.debug-flag

--- src/utils/debug_flag.clj
+++
@@ -1,8 +0,0 @@
-(ns utils.debug-flag)
-
-(def flags [])
-
-(defmacro def-debug-flag [sym initial-value]
-  `(do
-    (def ~(with-meta sym {:private true}) ~initial-value)
-    (alter-var-root #'flags conj (var ~sym))))
