0ff78b04c7eb0f2b55818bbe40f8a039eb4c7fc1
Michael Sappler <michaelsappler@gmail.com>
2024-08-09 16:17:57 +0530

    Document api.tx/transact!

--- src/api/tx.clj
+++ src/api/tx.clj
@@ -1,11 +1,17 @@
 (ns api.tx
   (:require [core.component :refer [defsystem]]))
 
-; 1. return new ctx if we change something or have side effect -> will be recorded
-; 2. return seq of txs if we do a nested transact-all!
-; 3. if we do side effects with transact-all! return empty vector [] instead of ctx
-; so will not be recorded double
-; => we record only real side effect-y txs
+; 1. return new ctx if we change something in the ctx or have side effect -> will be recorded
+; when returning a 'map?'
+
+; 2. return seq of txs -> those txs will be done recursively
+
+; 3. return nil in case of doing nothing -> will just continue with existing ctx.
+
+; do NOT do a transact-all! inside a transact! because then we have to return a context
+; and that means that transaction will be recorded and done double with all the sub-transactions
+; in the replay mode
+; we only want to record actual side effects, not transactions returning other lower level transactions
 (defsystem transact! [_ ctx])
 
 (comment
