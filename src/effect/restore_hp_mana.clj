ccaff52041771d0b2eaba9c19ef08664ff5d1c13
Michael Sappler <michaelsappler@gmail.com>
2024-08-11 19:52:48 +0530

    Add idea for common effects

--- src/effect/restore_hp_mana.clj
+++ src/effect/restore_hp_mana.clj
@@ -22,3 +22,35 @@
 
 (def-set-to-max-effect :entity/hp)
 (def-set-to-max-effect :entity/mana)
+
+; effect
+; key       - :entity/hp
+; operation - set-to-max (because val-max)
+; value = here no value
+
+#_(defcomponent :effect/set-to-max {:widget :label
+                                  :schema [:= true]
+                                  :default-value true}
+  (effect/text [[_ stat] _effect-ctx]
+    (str "Sets " (name stat) " to max."))
+
+  (effect/valid-params? [_ {:keys [effect/source]}]
+    source)
+
+  (effect/useful? [[_ stat] {:keys [effect/source]} _ctx]
+    (lower-than-max? (stat @source)))
+
+  (effect/txs [[_ stat] {:keys [effect/source]}]
+    [[:tx.entity/assoc source stat (set-to-max (stat @source))]]))
+
+; this as macro ... ? component which sets the value of another component ??
+#_(defcomponent :effect/set-mana-to-max {:widget :label
+                                         :schema [:= true]
+                                         :default-value true}
+  (effect/valid-params? [_ {:keys [effect/source]}] source)
+  (effect/text    [_ _effect-ctx]     (effect/text    [:effect/set-to-max :entity/mana]))
+  (effect/useful? [_ effect-ctx _ctx] (effect/useful? [:effect/set-to-max :entity/mana] effect-ctx))
+  (effect/txs     [_ effect-ctx]      (effect/txs     [:effect/set-to-max :entity/mana] effect-ctx)))
+
+#_[:effect/set-to-max :entity/hp]
+#_[:effect/set-to-max :entity/mana]
