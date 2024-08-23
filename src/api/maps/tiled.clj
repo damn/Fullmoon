e792f0433b17ef9c0f059c5c9a0652e9ce4a6fa3
Michael Sappler <michaelsappler@gmail.com>
2024-08-23 07:43:14 +0530

    context.tiled-map-renderer

--- src/api/maps/tiled.clj
+++ src/api/maps/tiled.clj
@@ -1,5 +1,8 @@
 (ns api.maps.tiled
-  (:import com.badlogic.gdx.maps.MapLayer))
+  (:require [gdx.maps.map-properties :as properties]
+            [gdx.maps.map-layers :as map-layers])
+  (:import com.badlogic.gdx.maps.MapLayer
+           [com.badlogic.gdx.maps.tiled TiledMap TiledMapTile TiledMapTileLayer TiledMapTileLayer$Cell]))
 
 ; implemented by: TiledMap, TiledMapTile, TiledMapTileLayer
 (defprotocol HasProperties
@@ -34,6 +37,76 @@
   (if (keyword? layer)
     (name layer)
     (.getName ^MapLayer layer)))
+
+(comment
+ ; could do this but slow -> fetch directly necessary properties
+ (defn properties [obj]
+   (let [^MapProperties ps (.getProperties obj)]
+     (zipmap (map keyword (.getKeys ps)) (.getValues ps))))
+
+ )
+
+(defn- lookup [has-properties key]
+  (properties/get (properties has-properties) (name key)))
+
+(extend-protocol HasProperties
+  TiledMap
+  (properties [tiled-map] (.getProperties tiled-map))
+  (get-property [tiled-map key] (lookup tiled-map key))
+
+  MapLayer
+  (properties [layer] (.getProperties layer))
+  (get-property [layer key] (lookup layer key))
+
+  TiledMapTile
+  (properties [tile] (.getProperties tile))
+  (get-property [tile key] (lookup tile key)))
+
+(extend-type TiledMap
+  com.badlogic.gdx.maps.tiled.TiledMap
+  (width  [tiled-map] (get-property tiled-map :width))
+  (height [tiled-map] (get-property tiled-map :height))
+
+  (layers [tiled-map]
+    (.getLayers tiled-map))
+
+  (layer-index [tiled-map layer]
+    (map-layers/index (layers tiled-map)
+                      (layer-name layer)))
+
+  (get-layer [tiled-map layer-name]
+    (map-layers/get (layers tiled-map) layer-name))
+
+  (remove-layer! [tiled-map layer]
+    (map-layers/remove! (layers tiled-map)
+                        (layer-index tiled-map layer)))
+
+  (cell-at [tiled-map layer [x y]]
+    (when-let [layer (get-layer tiled-map (layer-name layer))]
+      (.getCell ^TiledMapTileLayer layer x y)))
+
+  ; we want cell property not tile property
+  ; so why care for no-cell ? just return nil
+  (property-value [tiled-map layer position property-key]
+    (assert (keyword? property-key))
+    (if-let [cell (cell-at tiled-map layer position)]
+      (if-let [value (get-property (.getTile ^TiledMapTileLayer$Cell cell) property-key)]
+        value
+        :undefined)
+      :no-cell))
+
+  (map-positions [tiled-map]
+    (for [x (range (width  tiled-map))
+          y (range (height tiled-map))]
+      [x y]))
+
+  (positions-with-property [tiled-map layer property-key]
+    (when (layer-index tiled-map layer)
+      (for [position (map-positions tiled-map)
+            :let [[x y] position
+                  value (property-value tiled-map layer position property-key)]
+            :when (not (#{:undefined :no-cell} value))]
+        [position value]))))
 
 ; TODO performance bottleneck -> every time getting same layers
 ; takes 600 ms to read movement-properties
---
+++ src/context/tiled_map_renderer.clj
@@ -0,0 +1,46 @@
+(ns context.tiled-map-renderer
+  (:require [gdx.maps.map-renderer :as map-renderer]
+            [gdx.maps.map-layer :as map-layer]
+            [gdx.maps.tiled.tmx-map-loader :refer [->tmx-map-loader] :as tmx-map-loader]
+            [core.component :refer [defcomponent] :as component]
+            [api.context :as ctx]
+            [api.graphics :as g]
+            [api.maps.tiled :as tiled])
+  (:import [gdl OrthogonalTiledMapRenderer ColorSetter]))
+
+; OrthogonalTiledMapRenderer extends BatchTiledMapRenderer
+; and when a batch is passed to the constructor
+; we do not need to dispose the renderer
+(defn- map-renderer-for [{:keys [batch] :as g} tiled-map]
+  (OrthogonalTiledMapRenderer. tiled-map
+                               (float (g/world-unit-scale g))
+                               batch))
+
+(defn- set-color-setter! [^OrthogonalTiledMapRenderer map-renderer color-setter]
+  (.setColorSetter map-renderer (reify ColorSetter
+                                  (apply [_ color x y]
+                                    (color-setter color x y)))))
+
+(defcomponent :context/tiled-map-renderer {}
+  (component/create [_ _ctx]
+    (memoize map-renderer-for)))
+
+(extend-type api.context.Context
+  api.context/TiledMapLoader
+  (->tiled-map [_ file]
+    (tmx-map-loader/load (->tmx-map-loader) file))
+
+  (render-tiled-map [{g :context/graphics
+                      cached-map-renderer :context/tiled
+                      :as ctx}
+                     tiled-map
+                     color-setter]
+    (let [map-renderer (cached-map-renderer g tiled-map)
+          world-camera (ctx/world-camera ctx)]
+      (set-color-setter! map-renderer color-setter)
+      (map-renderer/set-view! map-renderer world-camera)
+      (->> tiled-map
+           tiled/layers
+           (filter map-layer/visible?)
+           (map (partial tiled/layer-index tiled-map))
+           (map-renderer/render map-renderer)))))
