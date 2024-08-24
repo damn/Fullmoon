1e8b7baeee4e46ba339b0c72abad1dd3861e3412
Michael Sappler <michaelsappler@gmail.com>
2024-08-24 20:48:02 +0530

    Indentation core.animation

--- src/core/animation.clj
+++ src/core/animation.clj
@@ -14,16 +14,18 @@
       (assoc this :cnt (cond (< newcnt maxcnt) newcnt
                              looping? (min maxcnt (- newcnt maxcnt))
                              :else maxcnt))))
+
   (restart [this]
     (assoc this :cnt 0))
+
   (stopped? [_]
     (and (not looping?) (>= cnt maxcnt)))
+
   (current-frame [this]
     (frames (min (int (/ (float cnt) (float frame-duration)))
                  (dec (count frames))))))
 
-(defn create
-  [frames & {:keys [frame-duration looping?]}]
+(defn create [frames & {:keys [frame-duration looping?]}]
   (map->ImmutableAnimation
     {:frames (vec frames)
      :frame-duration frame-duration
