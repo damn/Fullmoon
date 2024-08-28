aef37287463f0bb80ea2075da564e0a3a8b583d4
Michael Sappler <michaelsappler@gmail.com>
2024-08-28 11:31:48 +0530

    Remove comments

--- src/components/properties/audiovisual.clj
+++ src/components/properties/audiovisual.clj
@@ -8,15 +8,13 @@
      :schema [[:property/id [:qualified-keyword {:namespace :audiovisuals}]]
               [:property/sound
                :property/animation]]
-     :edn-file-sort-order 7 ; ? global put
+     :edn-file-sort-order 7
      :overview {:title "Audiovisuals"
                 :columns 10
-                :image/dimensions [96 96]} ; ??
-     }))
+                :image/dimensions [96 96]}}))
 
 (defcomponent :tx.entity/audiovisual
   (component/do! [[_ position id] ctx]
-    ; assert property of type audiovisual
     (let [{:keys [property/sound
                   property/animation]} (ctx/get-property ctx id)]
       [[:tx/sound sound]
