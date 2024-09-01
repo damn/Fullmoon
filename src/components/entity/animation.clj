e544b7297e25b2561f320effa6f5926c59606e56
Michael Sappler <michaelsappler@gmail.com>
2024-09-01 10:57:13 +0530

    Move entity/delete-after-animation-stopped to entity/animation

--- src/components/entity/animation.clj
+++ src/components/entity/animation.clj
@@ -16,3 +16,11 @@
   (component/tick [[k _] eid ctx]
     [(tx-assoc-image-current-frame eid animation)
      [:tx/assoc eid k (animation/tick animation (ctx/delta-time ctx))]]))
+
+(defcomponent :entity/delete-after-animation-stopped?
+  (component/create-e [_ entity _ctx]
+    (-> @entity :entity/animation :looping? not assert))
+
+  (component/tick [_ entity _ctx]
+    (when (animation/stopped? (:entity/animation @entity))
+      [[:tx/destroy entity]])))
