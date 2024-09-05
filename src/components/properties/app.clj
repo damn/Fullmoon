96c2ea6b0418fde9ceb559d5b65f94fbcd02d2c8
Michael Sappler <michaelsappler@gmail.com>
2024-09-05 08:19:33 +0530

    app context properties as vector because of dependencies

--- src/components/properties/app.clj
+++ src/components/properties/app.clj
@@ -14,15 +14,21 @@
 ; properties requires graphics (image)
 ; so cannot be [:components-ns :context] map but has to be a vector
 ;= > cannot change anything then VISUI not loaded.. then VISUI not loaded..
-(defcomponent :app/context {:data [:map
-                                   [:context/assets
-                                    :context/background-image
-                                    :context/config
-                                    :context/graphics
-                                    :context/properties
-                                    :context/screens
-                                    [:context/vis-ui {:optional true}]
-                                    [:context/tiled-map-renderer {:optional true}]]]})
+; if I save something else app/context will get messed up right? so has to be vector?
+(defcomponent :app/context
+  {:data :some
+   #_[:map
+      [:context/assets ; no deps
+       :context/background-image ; graphics for image
+       :context/config ; no deps
+       :context/graphics ; no deps
+       :context/properties ; no deps
+       :context/screens ; ? => properties, graphics, vis-ui, bg-image => move out of context?! but its ctx...
+       ; screens as properties actually?!?!
+       ; or vis-ui in graphics & tiled-map-renderer ?
+       [:context/vis-ui {:optional true}] ; no deps
+       [:context/tiled-map-renderer {:optional true}] ; no deps (but use graphics)
+       ]]})
 
 (defcomponent :properties/app
   (component/create [_ _ctx]
