f343bf5d1f1646ce17f812449894cd562116a61f
Michael Sappler <michaelsappler@gmail.com>
2024-09-08 08:15:20 +0530

    dev/post-txs!

--- src/dev.clj
+++ src/dev.clj
@@ -3,32 +3,41 @@
 
 (comment
 
- (gdx.app/post-runnable (fn []
-                          (do
-                           (learn-skill! :skills/projectile)
-                           (learn-skill! :skills/spawn)
-                           (learn-skill! :skills/meditation)
-                           (learn-skill! :skills/death-ray)
-                           (learn-skill! :skills/convert)
-                           (learn-skill! :skills/blood-curse)
-                           (learn-skill! :skills/slow)
-                           (learn-skill! :skills/double-fireball))))
+ ; * Test
+ ; * if z-order/effect renders behind wall
+ ; * => graphics txs?
+ (post-txs! [[:tx/line-render {:start [38 64]
+                               :end [40 70]
+                               :color [1 1 1]
+                               :duration 2}]])
+
+ (do
+  (learn-skill! :skills/projectile)
+  (learn-skill! :skills/spawn)
+  (learn-skill! :skills/meditation)
+  (learn-skill! :skills/death-ray)
+  (learn-skill! :skills/convert)
+  (learn-skill! :skills/blood-curse)
+  (learn-skill! :skills/slow)
+  (learn-skill! :skills/double-fireball))
 
  (create-item! :items/blood-glove)
 
  )
 
-(defn- do-on-ctx! [tx-fn]
-  (swap! app/state ctx/do! [(tx-fn @app/state)]))
+
+(defn- post-txs! [txs]
+  (gdx.app/post-runnable
+   #(swap! app/state ctx/do! txs)))
 
 (defn learn-skill! [skill-id]
-  (do-on-ctx! (fn [ctx]
-                [:tx/add-skill
+  (post-txs! (fn [ctx]
+               [[:tx/add-skill
                  (:entity/id (ctx/player-entity* ctx))
-                 (ctx/property ctx skill-id)])))
+                 (ctx/property ctx skill-id)]])))
 
 (defn create-item! [item-id]
-  (do-on-ctx! (fn [ctx]
-                [:tx/item
+  (post-txs! (fn [ctx]
+               [[:tx/item
                  (:position (ctx/player-entity* ctx))
-                 (ctx/property ctx item-id)])))
+                 (ctx/property ctx item-id)]])))
