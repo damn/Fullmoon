856935f8f73678832749e05d90515c3f441756aa
Michael Sappler <michaelsappler@gmail.com>
2024-09-11 20:56:49 +0530

    Deactivate setting of :world/player-creature

--- src/components/properties/world.clj
+++ src/components/properties/world.clj
@@ -3,7 +3,7 @@
             [core.context :as ctx]
             mapgen.gen))
 
-(defcomponent :world/player-creature {:data [:one-to-one :properties/creatures]})
+(defcomponent :world/player-creature {:data :some #_[:one-to-one :properties/creatures]})
 
 (defcomponent :world/map-size {:data :pos-int})
 (defcomponent :world/max-area-level {:data :pos-int}) ; TODO <= map-size !?
