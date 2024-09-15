7f9b750f560efcaa32df742847314753d6a66769
Michael Sappler <michaelsappler@gmail.com>
2024-09-15 09:21:57 +0530

    Delete gdx.files.file-handle

--- src/gdx/files/file_handle.clj
+++
@@ -1,8 +0,0 @@
-(ns gdx.files.file-handle
-  (:refer-clojure :exclude [list])
-  (:import com.badlogic.gdx.files.FileHandle))
-
-(defn list       [^FileHandle file] (.list        file))
-(defn directory? [^FileHandle file] (.isDirectory file))
-(defn extension  [^FileHandle file] (.extension   file))
-(defn path       [^FileHandle file] (.path        file))
--- src/utils/files.clj
+++ src/utils/files.clj
@@ -1,18 +1,18 @@
 (ns utils.files
-  (:require [gdx.files.file-handle :as file-handle])
-  (:import com.badlogic.gdx.Gdx))
+  (:import com.badlogic.gdx.Gdx
+           com.badlogic.gdx.files.FileHandle))
 
 (defn recursively-search [folder extensions]
-  (loop [[file & remaining] (file-handle/list (.internal Gdx/files file))
+  (loop [[^FileHandle file & remaining] (.list (.internal Gdx/files folder))
          result []]
     (cond (nil? file)
           result
 
-          (file-handle/directory? file)
-          (recur (concat remaining (file-handle/list file)) result)
+          (.isDirectory file)
+          (recur (concat remaining (.list file)) result)
 
-          (extensions (file-handle/extension file))
-          (recur remaining (conj result (file-handle/path file)))
+          (extensions (.extension file))
+          (recur remaining (conj result (.path file)))
 
           :else
           (recur remaining result))))
