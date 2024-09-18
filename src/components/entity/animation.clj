f9a508806eb6d49e23b0cf3feaad1e50cee639e1
Michael Sappler <michaelsappler@gmail.com>
2024-09-18 05:02:21 +0200

    Fix animation alias not needed

--- src/components/entity/animation.clj
+++ src/components/entity/animation.clj
@@ -62,7 +62,7 @@
                :cell-defaults {:pad 1}}))
 
 (defn- tx-assoc-image-current-frame [eid animation]
-  [:tx/assoc eid :entity/image (animation/current-frame animation)])
+  [:tx/assoc eid :entity/image (current-frame animation)])
 
 (defcomponent :entity/animation
   {:data :data/animation
@@ -72,12 +72,12 @@
 
   (component/tick [[k _] eid ctx]
     [(tx-assoc-image-current-frame eid animation)
-     [:tx/assoc eid k (animation/tick animation (ctx/delta-time ctx))]]))
+     [:tx/assoc eid k (tick animation (ctx/delta-time ctx))]]))
 
 (defcomponent :entity/delete-after-animation-stopped?
   (component/create-e [_ entity _ctx]
     (-> @entity :entity/animation :looping? not assert))
 
   (component/tick [_ entity _ctx]
-    (when (animation/stopped? (:entity/animation @entity))
+    (when (stopped? (:entity/animation @entity))
       [[:tx/destroy entity]])))
