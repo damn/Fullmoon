99b74dbeddc4e603ee69b205b8efb6fa08c23bdc
Michael Sappler <michaelsappler@gmail.com>
2024-09-21 08:20:05 +0200

    Add docstrings to core.effect

--- src/core/effect.clj
+++ src/core/effect.clj
@@ -1,10 +1,18 @@
 (ns core.effect
   (:require [core.component :refer [defsystem]]))
 
-(defsystem applicable? "? TODO. Required system, no default."[_ ctx])
+(defsystem applicable?
+  "An effect will only be done (with tx/do!) if this function returns truthy.
+Required system for every effect, no default."
+  [_ ctx])
 
-(defsystem useful? "Used for AI. Default true. "[_ ctx])
+(defsystem useful?
+  "Used for NPC AI.
+Called only if applicable? is truthy.
+For example use for healing effect is only useful if hitpoints is < max.
+Default method returns true."
+  [_ ctx])
 (defmethod useful? :default [_ ctx] true)
 
-(defsystem render "? renders effect while active till done?. Default do nothing." [_ g ctx])
+(defsystem render "Renders effect during active-skill state while active till done?. Default do nothing." [_ g ctx])
 (defmethod render :default [_ g ctx])
