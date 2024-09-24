9fce3a6b5882ca02d1ff5201dd4229a01272b36e
Michael Sappler <michaelsappler@gmail.com>
2024-09-25 01:18:52 +0200

    eat operation

--- src/core/stats.clj
+++ src/core/stats.clj
@@ -1,10 +1,95 @@
 (ns core.stats
   (:require [clojure.string :as str]
+            [clojure.math :as math]
             #_[malli.core :as m]
             [core.ctx :refer :all]
-            [core.entity :as entity]
-            [core.operation :as operation])
+            [core.entity :as entity])
   (:import com.badlogic.gdx.graphics.Color))
+
+(defsystem op-value-text "FIXME" [_])
+(defsystem op-apply "FIXME" [_ base-value])
+(defsystem op-order "FIXME" [_])
+
+(defn- +? [n]
+  (case (math/signum n)
+    0.0 ""
+    1.0 "+"
+    -1.0 ""))
+
+(defn- op-info-text [{value 1 :as operation}]
+  (str (+? value) (op-value-text operation)))
+
+(defcomponent :op/inc
+  {:data :number
+   :let value}
+  (op-value-text [_] (str value))
+  (op-apply [_ base-value] (+ base-value value))
+  (op-order [_] 0))
+
+(defcomponent :op/mult
+  {:data :number
+   :let value}
+  (op-value-text [_] (str (int (* 100 value)) "%"))
+  (op-apply [_ base-value] (* base-value (inc value)))
+  (op-order [_] 1))
+
+(defn- ->pos-int [v]
+  (-> v int (max 0)))
+
+(defn- val-max-op-k->parts [op-k]
+  (let [[val-or-max inc-or-mult] (mapv keyword (str/split (name op-k) #"-"))]
+    [val-or-max (keyword "op" (name inc-or-mult))]))
+
+(comment
+ (= (val-max-op-k->parts :op/val-inc) [:val :op/inc])
+ )
+
+(defcomponent :op/val-max
+  (op-value-text [[op-k value]]
+    (let [[val-or-max op-k] (val-max-op-k->parts op-k)]
+      (str (op-value-text [op-k value]) " " (case val-or-max
+                                              :val "Minimum"
+                                              :max "Maximum"))))
+
+
+  (op-apply [[operation-k value] val-max]
+    (assert (m/validate val-max-schema val-max) (pr-str val-max))
+    (let [[val-or-max op-k] (val-max-op-k->parts operation-k)
+          f #(op-apply [op-k value] %)
+          [v mx] (update val-max (case val-or-max :val 0 :max 1) f)
+          v  (->pos-int v)
+          mx (->pos-int mx)
+          vmx (case val-or-max
+                :val [v (max v mx)]
+                :max [(min v mx) mx])]
+      (assert (m/validate val-max-schema vmx))
+      vmx))
+
+  (op-order [[op-k value]]
+    (let [[_ op-k] (val-max-op-k->parts op-k)]
+      (op-order [op-k value]))))
+
+(defcomponent :op/val-inc {:data :int})
+(derive       :op/val-inc :op/val-max)
+
+(defcomponent :op/val-mult {:data :number})
+(derive       :op/val-mult :op/val-max)
+
+(defcomponent :op/max-inc {:data :int})
+(derive       :op/max-inc :op/val-max)
+
+(defcomponent :op/max-mult {:data :number})
+(derive       :op/max-mult :op/val-max)
+
+(comment
+ (and
+  (= (op-apply [:op/val-inc 30]    [5 10]) [35 35])
+  (= (op-apply [:op/max-mult -0.5] [5 10]) [5 5])
+  (= (op-apply [:op/val-mult 2]    [5 10]) [15 15])
+  (= (op-apply [:op/val-mult 1.3]  [5 10]) [11 11])
+  (= (op-apply [:op/max-mult -0.8] [5 10]) [1 1])
+  (= (op-apply [:op/max-mult -0.9] [5 10]) [0 0]))
+ )
 
 (defn- txs-update-modifiers [entity modifiers f]
   (for [[modifier-k operations] modifiers
@@ -70,7 +155,7 @@
        (str/join "\n"
                  (for [[modifier-k operations] modifiers
                        operation operations]
-                   (str (operation/op-info-text operation) " " (k->pretty-name modifier-k))))
+                   (str (op-info-text operation) " " (k->pretty-name modifier-k))))
        "[]"))
 
 (defcomponent :entity/modifiers
@@ -92,9 +177,9 @@
     {:pre [(= "modifier" (namespace modifier-k))]}
     (->> modifiers
          modifier-k
-         (sort-by operation/order)
+         (sort-by op-order)
          (reduce (fn [base-value [operation-k values]]
-                   (operation/apply [operation-k (apply + values)] base-value))
+                   (op-apply [operation-k (apply + values)] base-value))
                  base-value))))
 
 (comment
@@ -146,7 +231,7 @@
   (info-text [[k operations] _effect-ctx]
     (str/join "\n"
               (for [operation operations]
-                (str (operation/op-info-text operation) " " (k->pretty-name k)))))
+                (str (op-info-text operation) " " (k->pretty-name k)))))
 
   (applicable? [[k _] {:keys [effect/target]}]
     (and target
@@ -160,9 +245,9 @@
       (when-let [effective-value (entity/stat @target stat-k)]
         [[:e/assoc-in target [:entity/stats stat-k]
           ; TODO similar to components.entity.modifiers/->modified-value
-          ; but operations not sort-by operation/order ??
-          ; operation/apply reuse fn over operations to get effectiv value
-          (reduce (fn [value operation] (operation/apply operation value))
+          ; but operations not sort-by op/order ??
+          ; op-apply reuse fn over operations to get effectiv value
+          (reduce (fn [value operation] (op-apply operation value))
                   effective-value
                   operations)]]))))
 
