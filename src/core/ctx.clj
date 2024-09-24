649bda398b27982e1111d8d8b919fa257846b350
Michael Sappler <michaelsappler@gmail.com>
2024-09-24 22:52:32 +0200

    Fix duplicated current-screen

--- src/core/ctx.clj
+++ src/core/ctx.clj
@@ -703,25 +703,29 @@ Default method returns true."
     ; TODO screens not disposed https://github.com/damn/core/issues/41
     ))
 
-(defn current-screen-key [{{:keys [current-screen]} :context/screens}]
-  current-screen)
+(defn current-screen-key [{{:keys [current]} :context/screens}]
+  current)
 
-(defn current-screen [{{:keys [current-screen screens]} :context/screens}]
-  [current-screen (get screens current-screen)])
+(defn current-screen [{{:keys [current screens]} :context/screens}]
+  [current (get screens current)])
 
 (defn change-screen
   "Calls screen-exit on the current-screen (if there is one).
   Throws AssertionError when the context does not have a new-screen.
   Calls screen-enter on the new screen and
   returns the context with current-screen set to new-screen."
-  [{{:keys [current-screen screens]} :context/screens :as context}
+  [{{:keys [current screens]} :context/screens :as context}
    new-screen-key]
-  (when-let [screen-v (and current-screen
-                           (current-screen screens))]
-    (screen-exit [current-screen screen-v] context))
+  (when-let [screen-v (and current
+                           (current screens))]
+    (screen-exit [current screen-v] context))
 
   (let [screen-v (new-screen-key screens)
         _ (assert screen-v (str "Cannot find screen with key: " new-screen-key))
-        new-context (assoc-in context [:context/screens :current-screen] new-screen-key)]
+        new-context (assoc-in context [:context/screens :current] new-screen-key)]
     (screen-enter [new-screen-key screen-v] new-context)
     new-context))
+
+(defn ns-publics-comma-separated []
+  (apply str (interpose " , " (map str (keys (ns-publics 'core.ctx))))))
+; TODO no anonym class, macros
