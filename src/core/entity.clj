8e62f91572c5c14bbaf3fc77f2178ea3f2d3cb7a
Michael Sappler <michaelsappler@gmail.com>
2024-09-24 21:23:28 +0200

    eat src/core/entity/temp_modifier.clj

--- src/core/entity.clj
+++ src/core/entity.clj
@@ -967,3 +967,37 @@
        [:e/assoc entity :entity/mouseover? true])
      (fn [ctx]
        (assoc ctx ctx-mouseover-entity entity))]))
+
+(defcomponent :entity/temp-modifier
+  {:let {:keys [counter modifiers]}}
+  (info-text [_ ctx]
+    (str "[LIGHT_GRAY]Spiderweb - remaining: " (readable-number (time/finished-ratio ctx counter)) "/1[]"))
+
+  (tick [[k _] eid ctx]
+    (when (time/stopped? ctx counter)
+      [[:e/dissoc eid k]
+       [:tx/reverse-modifiers eid modifiers]]))
+
+  (render-above [_ entity* g ctx]
+    (draw-filled-circle g (:position entity*) 0.5 [0.5 0.5 0.5 0.4])))
+
+(let [modifiers {:modifier/movement-speed {:op/mult -0.5}}
+      duration 5]
+  (defcomponent :effect.entity/spiderweb
+    {:data :some}
+    (info-text [_ _effect-ctx]
+      "Spiderweb slows 50% for 5 seconds."
+      ; modifiers same like item/modifiers has info-text
+      ; counter ?
+      )
+
+    (applicable? [_ {:keys [effect/source effect/target]}]
+      ; ?
+      true)
+
+    ; TODO stacking? (if already has k ?) or reset counter ? (see string-effect too)
+    (do! [_ {:keys [effect/source effect/target] :as ctx}]
+      (when-not (:entity/temp-modifier @target)
+        [[:tx/apply-modifiers target modifiers]
+         [:e/assoc target :entity/temp-modifier {:modifiers modifiers
+                                                 :counter (time/->counter ctx duration)}]]))))
