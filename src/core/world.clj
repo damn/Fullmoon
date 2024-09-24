d30e944751f1929bc11a713ed4506645b69bfa1f
Michael Sappler <michaelsappler@gmail.com>
2024-09-24 20:59:49 +0200

    eat action-bar

--- src/core/world.clj
+++ src/core/world.clj
@@ -17,7 +17,6 @@
             [core.entity.player :as player]
             [core.world.gen.gen :as level-generator]
             [core.widgets.error-modal :refer [error-window!]]
-            [core.widgets.action-bar :as action-bar]
             [core.widgets.inventory :as inventory]
             [core.math.geom :as geom]
             [core.math.raycaster :as raycaster]
@@ -25,7 +24,8 @@
             [core.world.grid :as grid]
             [core.world.time :as time]
             [core.world.potential-fields :as potential-fields])
-  (:import com.badlogic.gdx.Input$Keys))
+  (:import com.badlogic.gdx.Input$Keys
+           (com.badlogic.gdx.scenes.scene2d.ui Button ButtonGroup)))
 
 (def ^:private content-grid :context/content-grid)
 
@@ -455,7 +455,7 @@
     window))
 
 (defn- ->ui-actors [ctx widget-data]
-  [(ui/->table {:rows [[{:actor (action-bar/->build)
+  [(ui/->table {:rows [[{:actor (->action-bar)
                          :expand? true
                          :bottom? true}]]
                 :id :action-bar-table
@@ -469,9 +469,110 @@
    (ui/->actor {:draw draw-item-on-cursor})
    (->mk [:widgets/player-message] ctx)])
 
+(def ^:private image-scale 2)
+
+(defn- ->action-bar []
+  (let [group (ui/->horizontal-group {:pad 2 :space 2})]
+    (actor/set-id! group ::action-bar)
+    group))
+
+(defn- ->action-bar-button-group []
+  (ui/->button-group {:max-check-count 1
+                      :min-check-count 0}))
+
+(defn- get-action-bar [ctx]
+  {:horizontal-group (::action-bar (:action-bar-table (stage/get ctx)))
+   :button-group (:action-bar (:context/widgets ctx))})
+
+(defcomponent :tx.action-bar/add
+  (do! [[_ {:keys [property/id entity/image] :as skill}] ctx]
+    (let [{:keys [horizontal-group button-group]} (get-action-bar ctx)
+          button (ui/->image-button image identity {:scale image-scale})]
+      (actor/set-id! button id)
+      (actor/add-tooltip! button
+                          #(->info-text skill (assoc % :effect/source (player-entity %))))
+      (group/add-actor! horizontal-group button)
+      (.add ^ButtonGroup button-group ^Button button)
+      ctx)))
+
+(defcomponent :tx.action-bar/remove
+  (do! [[_ {:keys [property/id]}] ctx]
+    (let [{:keys [horizontal-group button-group]} (get-action-bar ctx)
+          button (get horizontal-group id)]
+      (actor/remove! button)
+      (.remove ^ButtonGroup button-group ^Button button)
+      ctx)))
+
+(comment
+
+ (comment
+  (def sword-button (.getChecked button-group))
+  (.setChecked sword-button false)
+  )
+
+ #_(defn- number-str->input-key [number-str]
+     (eval (symbol (str "com.badlogic.gdx.Input$Keys/NUM_" number-str))))
+
+ ; TODO do with an actor
+ ; .getChildren horizontal-group => in order
+ (defn up-skill-hotkeys []
+   #_(doseq [slot slot-keys
+             :let [skill-id (slot @slot->skill-id)]
+             :when (and (.isKeyJustPressed gdx-input (number-str->input-key (name slot)))
+                        skill-id)]
+       (.setChecked ^Button (.findActor horizontal-group (str skill-id)) true)))
+
+ ; TODO
+ ; * cooldown / not usable -> diff. colors ? disable on not able to use skills (stunned?)
+ ; * or even sector circling for cooldown like in WoW (clipped !)
+ ; * tooltips ! with hotkey-number !
+ ;  ( (skills/text skill-id player-entity))
+ ; * add hotkey number to tooltips
+ ; * hotkeys => select button
+ ; when no selected-skill & new skill assoce'd (sword at start)
+ ; => set selected
+ ; keep weapon at position 1 always ?
+
+ #_(def ^:private slot-keys {:1 input.keys.num-1
+                             :2 input.keys.num-2
+                             :3 input.keys.num-3
+                             :4 input.keys.num-4
+                             :5 input.keys.num-5
+                             :6 input.keys.num-6
+                             :7 input.keys.num-7
+                             :8 input.keys.num-8
+                             :9 input.keys.num-9})
+
+ #_(defn- empty-slot->skill-id []
+     (apply sorted-map
+            (interleave slot-keys
+                        (repeat nil))))
+
+ #_(def selected-skill-id (atom nil))
+ #_(def ^:private slot->skill-id (atom nil))
+
+ #_(defn reset-skills! []
+     (reset! selected-skill-id nil)
+     (reset! slot->skill-id (empty-slot->skill-id)))
+
+
+ ; https://javadoc.io/doc/com.badlogicgames.gdx/gdx/latest/com/badlogic/gdx/scenes/scene2d/ui/Button.html
+ (.setProgrammaticChangeEvents ^Button (.findActor horizontal-group ":skills/spawn") true)
+ ; but doesn't toggle:
+ (.toggle ^Button (.findActor horizontal-group ":skills/spawn"))
+ (.setChecked ^Button (.findActor horizontal-group ":skills/spawn") true)
+ ; Toggles the checked state. This method changes the checked state, which fires a ChangeListener.ChangeEvent (if programmatic change events are enabled), so can be used to simulate a button click.
+
+ ; => it _worked_ => active skill changed
+ ; only button is not highlighted idk why
+
+ (.getChildren horizontal-group)
+
+ )
+
 (defcomponent :context/widgets
   (->mk [_ ctx]
-    (let [widget-data {:action-bar (action-bar/->button-group)
+    (let [widget-data {:action-bar (->action-bar-button-group)
                        :slot->background (inventory/->data ctx)}
           stage (stage/get ctx)]
       (.clear stage)
