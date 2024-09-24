df182da2ed41e5c4fdce0ab0f112367daf5d3ed2
Michael Sappler <michaelsappler@gmail.com>
2024-09-24 17:26:14 +0200

    screens/world -> world

--- src/core/world.clj
+++ src/core/world.clj
@@ -2,8 +2,24 @@
   (:require [core.utils.core :refer [tile->middle]]
             [core.ctx :refer :all]
             [core.tiled :as tiled]
+            [core.screen :as screen]
+            [core.screens :as screens]
+            [core.graphics.cursors :as cursors]
+            [core.graphics.camera :as camera]
+            [core.entity :as entity]
+            [core.entity.player :as player]
+            [core.stage :as stage]
+            [core.widgets.error-modal :refer [error-window!]]
+            [core.world.ecs :as ecs]
+            [core.world.widgets :as widgets]
             [core.world.content-grid :as content-grid]
-            [core.world.grid :as grid]))
+            [core.world.grid :as grid]
+            [core.world.mouseover-entity :refer [update-mouseover-entity]]
+            [core.world.time :as time]
+            [core.world.potential-fields :as potential-fields]
+            [core.world.render.tiled-map :as world-render]
+            [core.world.render.debug :as debug-render]  )
+  (:import com.badlogic.gdx.Input$Keys))
 
 (def ^:private ^:dbg-flag spawn-enemies? true)
 
@@ -102,3 +118,139 @@
     (content-grid/update-entity! ctx entity)
     (grid/entity-position-changed! ctx entity)
     ctx))
+
+(def ^:private ^:dbg-flag pausing? true)
+
+(defn- player-unpaused? []
+  (or (.isKeyJustPressed gdx-input Input$Keys/P)
+      (.isKeyPressed     gdx-input Input$Keys/SPACE)))
+
+(defn- update-game-paused [ctx]
+  (assoc ctx :context/paused? (or (:context/entity-tick-error ctx)
+                                  (and pausing?
+                                       (player/state-pause-game? ctx)
+                                       (not (player-unpaused?))))))
+
+(defn- update-world [ctx]
+  (let [ctx (time/update-time ctx (min (.getDeltaTime gdx-graphics) entity/max-delta-time))
+        active-entities (active-entities ctx)]
+    (potential-fields/update! ctx active-entities)
+    (try (ecs/tick-entities! ctx active-entities)
+         (catch Throwable t
+           (-> ctx
+               (error-window! t)
+               (assoc :context/entity-tick-error t))))))
+
+(defmulti ^:private game-loop :context/game-loop-mode)
+
+(defmethod game-loop :game-loop/normal [ctx]
+  (effect! ctx [player/update-state
+                update-mouseover-entity ; this do always so can get debug info even when game not running
+                update-game-paused
+                #(if (:context/paused? %)
+                   %
+                   (update-world %))
+                ecs/remove-destroyed-entities! ; do not pause this as for example pickup item, should be destroyed.
+                ]))
+
+(defn- replay-frame! [ctx]
+  (let [frame-number (time/logic-frame ctx)
+        txs [:foo]#_(ctx/frame->txs ctx frame-number)]
+    ;(println frame-number ". " (count txs))
+    (-> ctx
+        (effect! txs)
+        #_(update :world.time/logic-frame inc))))  ; this is probably broken now (also frame->txs contains already time, so no need to inc ?)
+
+(def ^:private replay-speed 2)
+
+(defmethod game-loop :game-loop/replay [ctx]
+  (reduce (fn [ctx _] (replay-frame! ctx))
+          ctx
+          (range replay-speed)))
+
+(defn- render-world! [ctx]
+  (camera/set-position! (world-camera ctx) (:position (player-entity* ctx)))
+  (world-render/render-map ctx (camera/position (world-camera ctx)))
+  (render-world-view ctx
+                     (fn [g]
+                       (debug-render/before-entities ctx g)
+                       (ecs/render-entities! ctx
+                                             g
+                                             (->> (active-entities ctx)
+                                                  (map deref)))
+                       (debug-render/after-entities ctx g))))
+
+(defn- adjust-zoom [camera by] ; DRY map editor
+  (camera/set-zoom! camera (max 0.1 (+ (camera/zoom camera) by))))
+
+(def ^:private zoom-speed 0.05)
+
+(defn- check-zoom-keys [ctx]
+  (let [camera (world-camera ctx)]
+    (when (.isKeyPressed gdx-input Input$Keys/MINUS)  (adjust-zoom camera    zoom-speed))
+    (when (.isKeyPressed gdx-input Input$Keys/EQUALS) (adjust-zoom camera (- zoom-speed)))))
+
+; TODO move to actor/stage listeners ? then input processor used ....
+(defn- check-key-input [ctx]
+  (check-zoom-keys ctx)
+  (widgets/check-window-hotkeys ctx)
+  (cond (and (.isKeyJustPressed gdx-input Input$Keys/ESCAPE)
+             (not (widgets/close-windows? ctx)))
+        (screens/change-screen ctx :screens/options-menu)
+
+        ; TODO not implementing StageSubScreen so NPE no screen/render!
+        #_(.isKeyJustPressed gdx-input Input$Keys/TAB)
+        #_(screens/change-screen ctx :screens/minimap)
+
+        :else
+        ctx))
+
+(defcomponent ::sub-screen
+  (screen/exit [_ ctx]
+    (cursors/set-cursor! ctx :cursors/default))
+
+  (screen/render [_ ctx]
+    (render-world! ctx)
+    (-> ctx
+        game-loop
+        check-key-input)))
+
+(derive :screens/world :screens/stage)
+(defcomponent :screens/world
+  (->mk [_ ctx]
+    {:stage (stage/create ctx [])
+     :sub-screen [::sub-screen]}))
+
+(comment
+
+ ; https://github.com/damn/core/issues/32
+ ; grep :game-loop/replay
+ ; unused code & not working
+ ; record only top-lvl txs or save world state every x minutes/seconds
+
+ ; TODO @replay-mode
+ ; * do I need check-key-input from screens/world?
+ ; adjust sound speed also equally ? pitch ?
+ ; player message, player modals, etc. all game related state handle ....
+ ; game timer is not reset  - continues as if
+ ; check other atoms , try to remove atoms ...... !?
+ ; replay mode no window hotkeys working
+ ; buttons working
+ ; can remove items from inventory ! changes cursor but does not change back ..
+ ; => deactivate all input somehow (set input processor nil ?)
+ ; works but ESC is separate from main input processor and on re-entry
+ ; again stage is input-processor
+ ; also cursor is from previous game replay
+ ; => all hotkeys etc part of stage input processor make.
+ ; set nil for non idle/item in hand states .
+ ; for some reason he calls end of frame checks but cannot open windows with hotkeys
+
+ (defn- start-replay-mode! [ctx]
+   (.setInputProcessor gdx-input nil)
+   (init-game-context ctx :mode :game-loop/replay))
+
+ (.postRunnable gdx-app
+  (fn []
+    (swap! app-state start-replay-mode!)))
+
+ )
