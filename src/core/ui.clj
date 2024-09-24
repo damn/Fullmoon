8d64db711b3ef67f1adaef323c24667003dc82d6
Michael Sappler <michaelsappler@gmail.com>
2024-09-24 21:29:37 +0200

    eat src/core/widgets/player_modal.clj

--- src/core/ui.clj
+++ src/core/ui.clj
@@ -354,3 +354,27 @@
                                :close-on-escape? true
                                :center? true
                                :pack? true})))
+
+; TODO no window movable type cursor appears here like in player idle
+; inventory still working, other stuff not, because custom listener to keypresses ? use actor listeners?
+; => input events handling
+; hmmm interesting ... can disable @ item in cursor  / moving / etc.
+
+(defn- show-player-modal! [ctx {:keys [title text button-text on-click]}]
+  (assert (not (::modal (stage/get ctx))))
+  (stage/add-actor! ctx
+                    (ui/->window {:title title
+                                  :rows [[(ui/->label text)]
+                                         [(ui/->text-button button-text
+                                                            (fn [ctx]
+                                                              (actor/remove! (::modal (stage/get ctx)))
+                                                              (on-click ctx)))]]
+                                  :id ::modal
+                                  :modal? true
+                                  :center-position [(/ (gui-viewport-width ctx) 2)
+                                                    (* (gui-viewport-height ctx) (/ 3 4))]
+                                  :pack? true})))
+
+(defcomponent :tx/player-modal
+  (do! [[_ params] ctx]
+    (show-player-modal! ctx params)))
