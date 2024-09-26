b169f34001eb0f33353693c74d9a36030f07227f
Michael Sappler <michaelsappler@gmail.com>
2024-09-26 11:55:57 +0200

    Move component data schemas together

--- src/clojure/world.clj
+++ src/clojure/world.clj
@@ -2200,8 +2200,11 @@ Returns ctx."
                              2)))))
  )
 
-(defcomponent :some {:schema :some})
-(defcomponent :boolean {:schema :boolean})
+;;;;
+
+(defn- add-schema-tooltip! [widget data]
+  (add-tooltip! widget (str "Schema: " (pr-str (m/form (:schema data)))))
+  widget)
 
 (defmethod ->widget :boolean [_ checked? _ctx]
   (assert (boolean? checked?))
@@ -2210,12 +2213,6 @@ Returns ctx."
 (defmethod widget->value :boolean [_ widget]
   (.isChecked ^VisCheckBox widget))
 
-(defcomponent :string {:schema :string})
-
-(defn- add-schema-tooltip! [widget data]
-  (add-tooltip! widget (str "Schema: " (pr-str (m/form (:schema data)))))
-  widget)
-
 (defmethod ->widget :string [[_ data] v _ctx]
   (add-schema-tooltip! (->text-field v {})
                        data))
@@ -2223,16 +2220,9 @@ Returns ctx."
 (defmethod widget->value :string [_ widget]
   (.getText ^VisTextField widget))
 
-(defcomponent :number  {:schema number?})
-(defcomponent :nat-int {:schema nat-int?})
-(defcomponent :int     {:schema int?})
-(defcomponent :pos     {:schema pos?})
-(defcomponent :pos-int {:schema pos-int?})
-
 (defn- ->edn-str [v]
   (binding [*print-level* nil]
     (pr-str v)))
-
 
 (defmethod ->widget :number [[_ data] v _ctx]
   (add-schema-tooltip! (->text-field (->edn-str v) {})
@@ -2241,20 +2231,12 @@ Returns ctx."
 (defmethod widget->value :number [_ widget]
   (edn/read-string (.getText ^VisTextField widget)))
 
-(defcomponent :enum
-  (->value [[_ items]]
-    {:schema (apply vector :enum items)}))
-
 (defmethod ->widget :enum [[_ data] v _ctx]
   (->select-box {:items (map ->edn-str (rest (:schema data)))
                     :selected (->edn-str v)}))
 
 (defmethod widget->value :enum [_ widget]
   (edn/read-string (.getSelected ^VisSelectBox widget)))
-
-(defcomponent :qualified-keyword
-  (->value [schema]
-    {:schema schema}))
 
 (defn- attribute-schema
   "Can define keys as just keywords or with properties like [:foo {:optional true}]."
@@ -2271,6 +2253,38 @@ Returns ctx."
 (defn- map-schema [ks]
   (apply vector :map {:closed true} (attribute-schema ks)))
 
+(defn- namespaced-ks [ns-name-k]
+  (filter #(= (name ns-name-k) (namespace %))
+          (keys component-attributes)))
+
+;;;; Component Data Schemas
+
+(defcomponent :some    {:schema :some})
+(defcomponent :boolean {:schema :boolean})
+(defcomponent :string  {:schema :string})
+(defcomponent :number  {:schema number?})
+(defcomponent :nat-int {:schema nat-int?})
+(defcomponent :int     {:schema int?})
+(defcomponent :pos     {:schema pos?})
+(defcomponent :pos-int {:schema pos-int?})
+(defcomponent :sound   {:schema :string})
+(defcomponent :val-max {:schema (m/form val-max-schema)})
+(defcomponent :image   {:schema [:map {:closed true}
+                                 [:file :string]
+                                 [:sub-image-bounds {:optional true} [:vector {:size 4} nat-int?]]]})
+(defcomponent :data/animation {:schema [:map {:closed true}
+                                        [:frames :some]
+                                        [:frame-duration pos?]
+                                        [:looping? :boolean]]})
+
+(defcomponent :enum
+  (->value [[_ items]]
+    {:schema (apply vector :enum items)}))
+
+(defcomponent :qualified-keyword
+  (->value [schema]
+    {:schema schema}))
+
 (defcomponent :map
   (->value [[_ ks]]
     {:schema (map-schema ks)}))
@@ -2279,18 +2293,19 @@ Returns ctx."
   (->value [[_ ks]]
     {:schema (map-schema (map (fn [k] [k {:optional true}]) ks))}))
 
-(defn- namespaced-ks [ns-name-k]
-  (filter #(= (name ns-name-k) (namespace %))
-          (keys component-attributes)))
-
 (defcomponent :components-ns
   (->value [[_ ns-name-k]]
     (->value [:map-optional (namespaced-ks ns-name-k)])))
 
-(defcomponent :image
-  {:schema [:map {:closed true}
-            [:file :string]
-            [:sub-image-bounds {:optional true} [:vector {:size 4} nat-int?]]]})
+(defcomponent :one-to-many
+  (->value [[_ property-type]]
+    {:schema [:set [:qualified-keyword {:namespace (property-type->id-namespace property-type)}]]}))
+
+(defcomponent :one-to-one
+  (->value [[_ property-type]]
+    {:schema [:qualified-keyword {:namespace (property-type->id-namespace property-type)}]}))
+
+;;;;
 
 (defmethod edn->value :image [_ image ctx]
   (edn->image image ctx))
@@ -2308,7 +2323,6 @@ Returns ctx."
                        #(stage-add! % (->scrollable-choose-window % (texture-rows %)))
                        {:dimensions [96 96]})) ; x2  , not hardcoded here
 
-(defcomponent :sound {:schema :string})
 
 (defn- ->scrollable-choose-window [ctx rows]
   (->window {:title "Choose"
@@ -2620,9 +2634,6 @@ Returns ctx."
 ; TODO schemas not checking if that property exists in db...
 ; https://github.com/damn/core/issues/59
 
-(defcomponent :one-to-many
-  (->value [[_ property-type]]
-    {:schema [:set [:qualified-keyword {:namespace (property-type->id-namespace property-type)}]]}))
 
 (defn- one-to-many-schema->linked-property-type [[_set [_qualif_kw {:keys [namespace]}]]]
   (ns-k->property-type namespace))
@@ -2635,9 +2646,6 @@ Returns ctx."
 (defmethod edn->value :one-to-many [_ property-ids ctx]
   (map (partial build-property ctx) property-ids))
 
-(defcomponent :one-to-one
-  (->value [[_ property-type]]
-    {:schema [:qualified-keyword {:namespace (property-type->id-namespace property-type)}]}))
 
 (defn- one-to-one-schema->linked-property-type [[_qualif_kw {:keys [namespace]}]]
   (ns-k->property-type namespace))
@@ -2735,7 +2743,6 @@ Returns ctx."
 (defmethod widget->value :one-to-one [_ widget]
   (->> (children widget) (keep actor-id) first))
 
-(defcomponent :val-max {:schema (m/form val-max-schema)})
 
 ;;;;️ 👾️ Entities
 
@@ -3042,11 +3049,6 @@ Returns ctx."
                :frame-duration frame-duration
                :looping? looping?))
 
-(defcomponent :data/animation
-  {:schema [:map {:closed true}
-            [:frames :some]
-            [:frame-duration pos?]
-            [:looping? :boolean]]})
 
 (defmethod edn->value :data/animation [_ animation ctx]
   (edn->animation animation ctx))
