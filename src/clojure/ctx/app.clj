c3ea324f7812b7ec55c880dc0aa4dd7da022e043
Michael Sappler <michaelsappler@gmail.com>
2024-09-27 13:07:32 +0200

    ctx/ app, operation, types

---
+++ src/clojure/ctx/app.clj
@@ -0,0 +1,89 @@
+(in-ns 'clojure.ctx)
+
+(declare ^{:tag com.badlogic.gdx.Graphics} gdx-graphics)
+
+(defn- bind-gdx-statics! []
+  (.bindRoot #'gdx-graphics Gdx/graphics))
+
+(defmacro post-runnable! [& exprs]
+  `(.postRunnable Gdx/app (fn [] ~@exprs)))
+
+(defn exit-app! []
+  (.exit Gdx/app))
+
+(defn- set-first-screen [context]
+  (->> context
+       :context/screens
+       :first-screen
+       (change-screen context)))
+
+(defn create-into
+  "For every component `[k v]`  `(->mk [k v] ctx)` is non-nil
+  or false, assoc's at ctx k v"
+  [ctx components]
+  (assert (map? ctx))
+  (reduce (fn [ctx [k v]]
+            (if-let [v (->mk [k v] ctx)]
+              (assoc ctx k v)
+              ctx))
+          ctx
+          components))
+
+(defn- ->application-listener [context]
+  (proxy [ApplicationAdapter] []
+    (create []
+      (bind-gdx-statics!)
+      (->> context
+           ; screens require vis-ui / properties (map-editor, property editor uses properties)
+           (sort-by (fn [[k _]] (if (= k :context/screens) 1 0)))
+           (create-into context)
+           set-first-screen
+           (reset! app-state)))
+
+    (dispose []
+      (run! destroy! @app-state))
+
+    (render []
+      (ScreenUtils/clear Color/BLACK)
+      (screen-render! (current-screen @app-state)))
+
+    (resize [w h]
+      ; TODO fix mac screen resize bug again
+      (on-resize @app-state w h))))
+
+(defn- ->lwjgl3-app-config [{:keys [title width height full-screen? fps]}]
+  ; can remove :pre, we are having a schema now
+  ; move schema here too ?
+  {:pre [title
+         width
+         height
+         (boolean? full-screen?)
+         (or (nil? fps) (int? fps))]}
+  ; https://github.com/libgdx/libgdx/pull/7361
+  ; Maybe can delete this when using that new libgdx version
+  ; which includes this PR.
+  (when SharedLibraryLoader/isMac
+    (.set Configuration/GLFW_LIBRARY_NAME "glfw_async")
+    (.set Configuration/GLFW_CHECK_THREAD0 false))
+  (let [config (doto (Lwjgl3ApplicationConfiguration.)
+                 (.setTitle title)
+                 (.setForegroundFPS (or fps 60)))]
+    (if full-screen?
+      (.setFullscreenMode config (Lwjgl3ApplicationConfiguration/getDisplayMode))
+      (.setWindowedMode config width height))
+    ; See https://libgdx.com/wiki/graphics/querying-and-configuring-graphics
+    ; but makes no difference
+    #_com.badlogic.gdx.graphics.glutils.HdpiMode
+    #_(.setHdpiMode config #_HdpiMode/Pixels HdpiMode/Logical)
+    config))
+
+(defrecord Context [])
+
+(defn start-app!
+  "Validates all properties, then creates the context record and starts a libgdx application with the desktop (lwjgl3) backend.
+Sets [[app-state]] atom to the context."
+  [properties-edn-file]
+  (let [ctx (map->Context (->ctx-properties properties-edn-file))
+        app (build-property ctx :app/core)]
+    (Lwjgl3Application. (->application-listener (safe-merge ctx (:app/context app)))
+                        (->lwjgl3-app-config (:app/lwjgl3 app)))))
---
+++ src/clojure/ctx/types.clj
@@ -0,0 +1,130 @@
+(in-ns 'clojure.ctx)
+
+(defn def-attributes [& attributes-data]
+  {:pre [(even? (count attributes-data))]}
+  (doseq [[k data] (partition 2 attributes-data)]
+    (defcomponent* k {:data data})))
+
+(def-attributes
+  :tag [:enum [:dev :prod]]
+  :configs :some)
+
+(def-attributes
+  :views [:map [:gui-view :world-view]]
+  :gui-view [:map [:world-width :world-height]]
+  :world-view [:map [:tile-size :world-width :world-height]]
+  :world-width :pos-int
+  :world-height :pos-int
+  :tile-size :pos-int
+  :default-font [:map [:file :quality-scaling :size]]
+  :file :string
+  :quality-scaling :pos-int
+  :size :pos-int
+  :cursors :some)
+
+(def-attributes
+  :fps          :nat-int
+  :full-screen? :boolean
+  :width        :nat-int
+  :height       :nat-int
+  :title        :string
+  :app/lwjgl3 [:map [:fps
+                     :full-screen?
+                     :width
+                     :height
+                     :title]]
+  :app/context [:map [ctx-assets
+                      :context/config
+                      :context/graphics
+                      :context/screens
+                      :context/vis-ui
+                      :context/tiled-map-renderer]])
+
+(def-attributes
+  :body/width   :pos
+  :body/height  :pos
+  :body/flying? :boolean)
+
+;;;; def-type
+
+(defn def-type [k {:keys [schema overview]}]
+  (defcomponent k
+    {:data [:map (conj schema :property/id)]
+     :overview overview}))
+
+(def-type :properties/app
+  {:schema [:app/lwjgl3
+            :app/context]
+   :overview {:title "Apps" ; - only 1 ? - no overview - ?
+              :columns 10}})
+
+(def-type :properties/audiovisuals
+  {:schema [:tx/sound
+            :entity/animation]
+   :overview {:title "Audiovisuals"
+              :columns 10
+              :image/scale 2}})
+
+(def-type :properties/items
+  {:schema [:property/pretty-name
+            :entity/image
+            :item/slot
+            [:item/modifiers {:optional true}]]
+   :overview {:title "Items"
+              :columns 20
+              :image/scale 1.1
+              :sort-by-fn #(vector (if-let [slot (:item/slot %)]
+                                     (name slot)
+                                     "")
+                             (name (:property/id %)))}})
+
+(def-type :properties/creatures
+  {:schema [:entity/body
+            :property/pretty-name
+            :creature/species
+            :creature/level
+            :entity/animation
+            :entity/stats
+            :entity/skills
+            [:entity/modifiers {:optional true}]
+            [:entity/inventory {:optional true}]]
+   :overview {:title "Creatures"
+              :columns 15
+              :image/scale 1.5
+              :sort-by-fn #(vector (:creature/level %)
+                                   (name (:creature/species %))
+                                   (name (:property/id %)))
+              :extra-info-text #(str (:creature/level %))}})
+
+(def-type :properties/projectiles
+  {:schema [:entity/image
+            :projectile/max-range
+            :projectile/speed
+            :projectile/piercing?
+            :entity-effects]
+   :overview {:title "Projectiles"
+              :columns 16
+              :image/scale 2}})
+
+(def-type :properties/skills
+  {:schema [:entity/image
+            :property/pretty-name
+            :skill/action-time-modifier-key
+            :skill/action-time
+            :skill/start-action-sound
+            :skill/effects
+            [:skill/cooldown {:optional true}]
+            [:skill/cost {:optional true}]]
+   :overview {:title "Skills"
+              :columns 16
+              :image/scale 2}})
+
+(def-type :properties/worlds
+  {:schema [:world/generator
+            :world/player-creature
+            [:world/tiled-map {:optional true}]
+            [:world/map-size {:optional true}]
+            [:world/max-area-level {:optional true}]
+            [:world/spawn-rate {:optional true}]]
+   :overview {:title "Worlds"
+              :columns 10}})
