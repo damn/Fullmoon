6bc708b2b9254fe578421325d2ef887f747b4bd9
Michael Sappler <michaelsappler@gmail.com>
2024-09-28 21:55:08 +0200

    Move render-tm! to clojure.gdx.tiled

--- src/core/world.clj
+++ src/core/world.clj
@@ -3,39 +3,28 @@
             [clojure.gdx :refer :all :exclude [visible?]]
             [clojure.gdx.tiled :refer :all]
             [clojure.string :as str]
-            [data.grid2d :as g])
-  (:import (gdl OrthogonalTiledMapRenderer ColorSetter)))
-
-; OrthogonalTiledMapRenderer extends BatchTiledMapRenderer
-; and when a batch is passed to the constructor
-; we do not need to dispose the renderer
-(defn ->tiled-map-renderer [{:keys [batch] :as g} tiled-map]
-  (OrthogonalTiledMapRenderer. tiled-map (float (world-unit-scale g)) batch))
-
-(defn- set-color-setter! [^OrthogonalTiledMapRenderer map-renderer color-setter]
-  (.setColorSetter map-renderer (reify ColorSetter
-                                  (apply [_ color x y]
-                                    (color-setter color x y)))))
+            [data.grid2d :as g]))
 
 (defn render!
   "Renders tiled-map using world-view at world-camera position and with world-unit-scale.
-Color-setter is a gdl.ColorSetter which is called for every tile-corner to set the color.
-Can be used for lights & shadows.
-The map-renderers are created and cached internally.
-Renders only visible layers."
-  [{g :context/graphics cached-map-renderer :context/tiled-map-renderer :as ctx}
+  Color-setter is a (fn [color x y]) which is called for every tile-corner to set the color.
+  Can be used for lights & shadows.
+  The map-renderers are created and cached internally.
+  Renders only visible layers."
+  [{g :context/graphics
+    cached-map-renderer :context/tiled-map-renderer
+    :as ctx}
    tiled-map
    color-setter]
-  (let [^OrthogonalTiledMapRenderer map-renderer (cached-map-renderer g tiled-map)
-        world-camera (world-camera ctx)]
-    (set-color-setter! map-renderer color-setter)
-    (.setView map-renderer world-camera)
-    (->> tiled-map
-         layers
-         (filter visible?)
-         (map (partial layer-index tiled-map))
-         int-array
-         (.render map-renderer))))
+  (render-tm! (cached-map-renderer g tiled-map)
+              color-setter
+              (world-camera ctx)
+              tiled-map))
+
+(defn ->tiled-map-renderer [{:keys [batch] :as g} tiled-map]
+  (->orthogonal-tiled-map-renderer tiled-map
+                                   (world-unit-scale g)
+                                   batch))
 
 (defcomponent :context/tiled-map-renderer
   {:data :some}
