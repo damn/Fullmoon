cec98b45bfa0bf50be5d7b8078f21d3bd23d6ecb
Michael Sappler <michaelsappler@gmail.com>
2024-09-28 22:37:53 +0200

    Move defsystem do! to effect

--- src/clojure/ctx/effect.clj
+++ src/clojure/ctx/effect.clj
@@ -18,6 +18,21 @@
 
 (declare effect!)
 
+(defsystem do!
+  " 1. return new ctx if we change something in the ctx or have side effect -> will be recorded
+  when returning a 'map?'
+
+  2. return seq of txs -> those txs will be done recursively
+  2.1 also seq of fns wih [ctx] param can be passed.
+
+  3. return nil in case of doing nothing -> will just continue with existing ctx.
+
+  do NOT do a effect/do inside a effect/do! because then we have to return a context
+  and that means that transaction will be recorded and done double with all the sub-transactions
+  in the replay mode
+  we only want to record actual side effects, not transactions returning other lower level transactions"
+  [_ ctx])
+
 (defn- handle-tx! [ctx tx]
   (let [result (if (fn? tx)
                  (tx ctx)
