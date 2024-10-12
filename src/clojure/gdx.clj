252425d84f783ee39bfa9866f5ecf81fa1d5e0ba
Michael Sappler <michaelsappler@gmail.com>
2024-10-12 21:51:02 +0200

    txs last

--- src/clojure/gdx.clj
+++ src/clojure/gdx.clj
@@ -120,30 +120,6 @@
                           :center? true
                           :pack? true})))
 
-; TODO no window movable type cursor appears here like in player idle
-; inventory still working, other stuff not, because custom listener to keypresses ? use actor listeners?
-; => input events handling
-; hmmm interesting ... can disable @ item in cursor  / moving / etc.
-
-(defn- show-player-modal! [{:keys [title text button-text on-click]}]
-  (assert (not (::modal (stage-get))))
-  (stage-add! (ui/window {:title title
-                          :rows [[(ui/label text)]
-                                 [(ui/text-button button-text
-                                                  (fn []
-                                                    (a/remove! (::modal (stage-get)))
-                                                    (on-click)))]]
-                          :id ::modal
-                          :modal? true
-                          :center-position [(/ (g/gui-viewport-width) 2)
-                                            (* (g/gui-viewport-height) (/ 3 4))]
-                          :pack? true})))
-
-(defc :tx/player-modal
-  (do! [[_ params]]
-    (show-player-modal! params)
-    nil))
-
 (defn enemy-faction [{:keys [entity/faction]}]
   (case faction
     :evil :good
@@ -1935,4 +1911,28 @@
 (defc :tx/cursor
   (do! [[_ cursor-key]]
     (g/set-cursor! cursor-key)
+    nil))
+
+; TODO no window movable type cursor appears here like in player idle
+; inventory still working, other stuff not, because custom listener to keypresses ? use actor listeners?
+; => input events handling
+; hmmm interesting ... can disable @ item in cursor  / moving / etc.
+
+(defn- show-player-modal! [{:keys [title text button-text on-click]}]
+  (assert (not (::modal (stage-get))))
+  (stage-add! (ui/window {:title title
+                          :rows [[(ui/label text)]
+                                 [(ui/text-button button-text
+                                                  (fn []
+                                                    (a/remove! (::modal (stage-get)))
+                                                    (on-click)))]]
+                          :id ::modal
+                          :modal? true
+                          :center-position [(/ (g/gui-viewport-width) 2)
+                                            (* (g/gui-viewport-height) (/ 3 4))]
+                          :pack? true})))
+
+(defc :tx/player-modal
+  (do! [[_ params]]
+    (show-player-modal! params)
     nil))
