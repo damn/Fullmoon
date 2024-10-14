ab6d1beb7cb9104bb45c03a7468c8981e618e52e
Michael Sappler <michaelsappler@gmail.com>
2024-10-14 23:23:17 +0200

    ➡️  Move modules and area-level-grid to world/generate/

--- src/world/generate.clj
+++ src/world/generate.clj
@@ -15,82 +15,11 @@
             [core.property :as property]
             [core.widgets.error :refer [error-window!]]
             [data.grid2d :as g2d]
-            [utils.core :refer [->tile]]))
-
-(def modules-file "maps/modules.tmx")
-(def module-width  32)
-(def module-height 20)
-(def modules-scale [module-width module-height])
-
-(def ^:private number-modules-x 8)
-(def ^:private number-modules-y 4)
-(def ^:private module-offset-tiles 1)
-(def ^:private transition-modules-row-width 4)
-(def ^:private transition-modules-row-height 4)
-(def ^:private transition-modules-offset-x 4)
-(def ^:private floor-modules-row-width 4)
-(def ^:private floor-modules-row-height 4)
-(def ^:private floor-idxvalue 0)
-
-(defn- module-index->tiled-map-positions [[module-x module-y]]
-  (let [start-x (* module-x (+ module-width  module-offset-tiles))
-        start-y (* module-y (+ module-height module-offset-tiles))]
-    (for [x (range start-x (+ start-x module-width))
-          y (range start-y (+ start-y module-height))]
-      [x y])))
-
-(defn- floor->module-index []
-  [(rand-int floor-modules-row-width)
-   (rand-int floor-modules-row-height)])
-
-(defn- transition-idxvalue->module-index [idxvalue]
-  [(+ (rem idxvalue transition-modules-row-width)
-      transition-modules-offset-x)
-   (int (/ idxvalue transition-modules-row-height))])
-
-(defn- place-module [scaled-grid
-                     unscaled-position
-                     & {:keys [transition?
-                               transition-neighbor?]}]
-  (let [idxvalue (if transition?
-                   (t/transition-idx-value unscaled-position transition-neighbor?)
-                   floor-idxvalue)
-        tiled-map-positions (module-index->tiled-map-positions
-                             (if transition?
-                               (transition-idxvalue->module-index idxvalue)
-                               (floor->module-index)))
-        offsets (for [x (range module-width)
-                      y (range module-height)]
-                  [x y])
-        offset->tiled-map-position (zipmap offsets tiled-map-positions)
-        scaled-position (mapv * unscaled-position modules-scale)]
-    (reduce (fn [grid offset]
-              (assoc grid
-                     (mapv + scaled-position offset)
-                     (offset->tiled-map-position offset)))
-            scaled-grid
-            offsets)))
-
-(defn place-modules [modules-tiled-map
-                     scaled-grid
-                     unscaled-grid
-                     unscaled-floor-positions
-                     unscaled-transition-positions]
-  (let [_ (assert (and (= (t/width modules-tiled-map)
-                          (* number-modules-x (+ module-width module-offset-tiles)))
-                       (= (t/height modules-tiled-map)
-                          (* number-modules-y (+ module-height module-offset-tiles)))))
-        scaled-grid (reduce (fn [scaled-grid unscaled-position]
-                              (place-module scaled-grid unscaled-position :transition? false))
-                            scaled-grid
-                            unscaled-floor-positions)
-        scaled-grid (reduce (fn [scaled-grid unscaled-position]
-                              (place-module scaled-grid unscaled-position :transition? true
-                                            :transition-neighbor? #(#{:transition :wall}
-                                                                    (get unscaled-grid %))))
-                            scaled-grid
-                            unscaled-transition-positions)]
-    (t/grid->tiled-map modules-tiled-map scaled-grid)))
+            [utils.core :refer [->tile]]
+            [world.generate.area-level-grid :as area-level-grid]
+            [world.generate.modules :as modules :refer [modules-scale
+                                                        module-width
+                                                        module-height]]))
 
 ; TODO generates 51,52. not max 50
 ; TODO can use different turn-ratio/depth/etc. params
@@ -178,6 +107,8 @@
           (when (seq creatures)
             (t/set-tile! layer position (creature->tile (rand-nth creatures)))))))))
 
+(def modules-file "maps/modules.tmx")
+
 (defn generate-modules
   "The generated tiled-map needs to be disposed."
   [{:keys [world/map-size
@@ -196,7 +127,7 @@
                   (str "(set (g2d/cells grid)): " (set (g2d/cells grid))))
         scale modules-scale
         scaled-grid (t/scale-grid grid scale)
-        tiled-map (place-modules (t/load-map modules-file)
+        tiled-map (modules/place (t/load-map modules-file)
                                  scaled-grid
                                  grid
                                  (filter #(= :ground     (get grid %)) (g2d/posis grid))
@@ -208,10 +139,11 @@
         ;_ (println "scaled grid with filled nil: '?' \n")
         ;_ (printgrid (reduce #(assoc %1 %2 nil) scaled-grid spawn-positions))
         ;_ (println "\n")
-        {:keys [steps area-level-grid]} (t/->area-level-grid :grid grid
-                                                             :start start
-                                                             :max-level max-area-level
-                                                             :walk-on #{:ground :transition})
+        {:keys [steps area-level-grid]} (area-level-grid/create
+                                         :grid grid
+                                         :start start
+                                         :max-level max-area-level
+                                         :walk-on #{:ground :transition})
         ;_ (printgrid area-level-grid)
         _ (assert (or
                    (= (set (concat [max-area-level] (range max-area-level)))
