dfc12220e37a60e3294c8e4f84565c26d5b57def
Michael Sappler <michaelsappler@gmail.com>
2024-10-16 06:21:29 +0200

    use entity, not entity*

--- src/world/entity.clj
+++ src/world/entity.clj
@@ -20,17 +20,17 @@
 (defsystem tick [_ eid])
 (defmethod tick :default [_ eid])
 
-(defsystem render-below [_ entity*])
-(defmethod render-below :default [_ entity*])
+(defsystem render-below [_ entity])
+(defmethod render-below :default [_ entity])
 
-(defsystem render [_ entity*])
-(defmethod render :default [_ entity*])
+(defsystem render [_ entity])
+(defmethod render :default [_ entity])
 
-(defsystem render-above [_ entity*])
-(defmethod render-above :default [_ entity*])
+(defsystem render-above [_ entity])
+(defmethod render-above :default [_ entity])
 
-(defsystem render-info [_ entity*])
-(defmethod render-info :default [_ entity*])
+(defsystem render-info [_ entity])
+(defmethod render-info :default [_ entity])
 
 (def render-systems [render-below render render-above render-info])
 
