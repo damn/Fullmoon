e4e98458db7877ed74ebc9fa179f84602341a2f8
Michael Sappler <michaelsappler@gmail.com>
2024-10-18 00:02:21 +0200

    Split up graphics_sd

--- src/gdx/graphics.clj
+++ src/gdx/graphics.clj
@@ -15,7 +15,8 @@
            (com.badlogic.gdx.graphics.g2d SpriteBatch TextureRegion)
            (com.badlogic.gdx.utils Disposable)
            (com.badlogic.gdx.utils.viewport FitViewport))
-  (:load "graphics/color"))
+  (:load "graphics/color"
+         "graphics_sd"))
 
 (defn- tr-dimensions [^TextureRegion texture-region]
   [(.getRegionWidth  texture-region)
@@ -33,55 +34,6 @@
 
   ([^TextureRegion texture-region [x y w h]]
    (TextureRegion. texture-region (int x) (int y) (int w) (int h))))
-
-(declare ^:private sd
-         ^:private sd-texture)
-
-(defn- sd-color [sd color]
-  (sd/set-color sd (munge-color color)))
-
-(defn draw-ellipse [position radius-x radius-y color]
-  (sd-color sd color)
-  (sd/ellipse sd position radius-x radius-y))
-
-(defn draw-filled-ellipse [position radius-x radius-y color]
-  (sd-color sd color)
-  (sd/filled-ellipse sd position radius-x radius-y))
-
-(defn draw-circle [position radius color]
-  (sd-color sd color)
-  (sd/circle sd position radius))
-
-(defn draw-filled-circle [position radius color]
-  (sd-color sd color)
-  (sd/filled-circle sd position radius))
-
-(defn draw-arc [center radius start-angle degree color]
-  (sd-color sd color)
-  (sd/arc sd center radius start-angle degree))
-
-(defn draw-sector [center radius start-angle degree color]
-  (sd-color sd color)
-  (sd/sector sd center radius start-angle degree))
-
-(defn draw-rectangle [x y w h color]
-  (sd-color sd color)
-  (sd/rectangle sd x y w h))
-
-(defn draw-filled-rectangle [x y w h color]
-  (sd-color sd color)
-  (sd/filled-rectangle sd x y w h))
-
-(defn draw-line [start end color]
-  (sd-color sd color)
-  (sd/line sd start end))
-
-(defn draw-grid [leftx bottomy gridw gridh cellw cellh color]
-  (sd-color sd color)
-  (sd/grid sd leftx bottomy gridw gridh cellw cellh))
-
-(defn with-shape-line-width [width draw-fn]
-  (sd/with-line-width sd width draw-fn))
 
 (defn- ->gui-view [{:keys [world-width world-height]}]
   {:unit-scale 1
