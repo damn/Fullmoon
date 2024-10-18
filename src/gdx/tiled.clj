dcb8778e3222227382cefcb0b7d5f49098965ac9
Michael Sappler <michaelsappler@gmail.com>
2024-10-18 03:24:10 +0200

    Remove unnecessary protocol

--- src/gdx/tiled.clj
+++ src/gdx/tiled.clj
@@ -45,76 +45,71 @@
   (m-props [tile] (.getProperties tile))
   (get-property [tile key] (lookup tile key)))
 
-(defprotocol TMap
-  (width [_])
-  (height [_])
-  (layers ^MapLayers [_] "Returns instance of com.badlogic.gdx.maps.MapLayers of the tiledmap")
-  (layer-index [_ layer]
-               "Returns nil or the integer index of the layer.
-               Layer can be keyword or an instance of TiledMapTileLayer.")
-  (get-layer [_ layer-name]
-             "Returns the layer with name (string).")
-  (remove-layer! [_ layer]
-                 "Removes the layer, layer can be keyword or an actual layer object.")
-  (cell-at [_ layer position] "Layer can be keyword or layer object.
-                              Position vector [x y].
-                              If the layer is part of tiledmap, returns the TiledMapTileLayer$Cell at position.")
-  (property-value [_ layer position property-key]
-                  "Returns the property value of the tile at the cell in layer.
-                  If there is no cell at this position in the layer returns :no-cell.
-                  If the property value is undefined returns :undefined.
-                  Layer is keyword or layer object.")
-  (map-positions [_] "Returns a sequence of all [x y] positions in the tiledmap.")
-  (positions-with-property [_ layer property-key]
-                           "If the layer (keyword or layer object) does not exist returns nil.
-                           Otherwise returns a sequence of [[x y] value] for all tiles who have property-key."))
+(defn width  [tiled-map] (get-property tiled-map :width))
+(defn height [tiled-map] (get-property tiled-map :height))
 
-(extend-type TiledMap
-  TMap
-  (width  [tiled-map] (get-property tiled-map :width))
-  (height [tiled-map] (get-property tiled-map :height))
+(defn layers ^MapLayers [tiled-map]
+  (.getLayers tiled-map))
 
-  (layers [tiled-map]
-    (.getLayers tiled-map))
+(defn layer-index
+  "Returns nil or the integer index of the layer.
+  Layer can be keyword or an instance of TiledMapTileLayer."
+  [tiled-map layer]
+  (let [idx (.getIndex (layers tiled-map) (layer-name layer))]
+    (when-not (= idx -1)
+      idx)))
 
-  (layer-index [tiled-map layer]
-    (let [idx (.getIndex (layers tiled-map) (layer-name layer))]
-      (when-not (= idx -1)
-        idx)))
+(defn get-layer
+  "Returns the layer with name (string)."
+  [tiled-map layer-name]
+  (.get (layers tiled-map) ^String layer-name))
 
-  (get-layer [tiled-map layer-name]
-    (.get (layers tiled-map) ^String layer-name))
+(defn remove-layer!
+  "Removes the layer, layer can be keyword or an actual layer object."
+  [tiled-map layer]
+  (.remove (layers tiled-map)
+           (int (layer-index tiled-map layer))))
 
-  (remove-layer! [tiled-map layer]
-    (.remove (layers tiled-map)
-             (int (layer-index tiled-map layer))))
+(defn cell-at
+  "Layer can be keyword or layer object.
+  Position vector [x y].
+  If the layer is part of tiledmap, returns the TiledMapTileLayer$Cell at position."
+  [tiled-map layer [x y]]
+  (when-let [layer (get-layer tiled-map (layer-name layer))]
+    (.getCell ^TiledMapTileLayer layer x y)))
 
-  (cell-at [tiled-map layer [x y]]
-    (when-let [layer (get-layer tiled-map (layer-name layer))]
-      (.getCell ^TiledMapTileLayer layer x y)))
+; we want cell property not tile property
+; so why care for no-cell ? just return nil
+(defn property-value
+  "Returns the property value of the tile at the cell in layer.
+  If there is no cell at this position in the layer returns :no-cell.
+  If the property value is undefined returns :undefined.
+  Layer is keyword or layer object."
+  [tiled-map layer position property-key]
+  (assert (keyword? property-key))
+  (if-let [cell (cell-at tiled-map layer position)]
+    (if-let [value (get-property (.getTile ^TiledMapTileLayer$Cell cell) property-key)]
+      value
+      :undefined)
+    :no-cell))
 
-  ; we want cell property not tile property
-  ; so why care for no-cell ? just return nil
-  (property-value [tiled-map layer position property-key]
-    (assert (keyword? property-key))
-    (if-let [cell (cell-at tiled-map layer position)]
-      (if-let [value (get-property (.getTile ^TiledMapTileLayer$Cell cell) property-key)]
-        value
-        :undefined)
-      :no-cell))
+(defn map-positions
+  "Returns a sequence of all [x y] positions in the tiledmap."
+  [tiled-map]
+  (for [x (range (width  tiled-map))
+        y (range (height tiled-map))]
+    [x y]))
 
-  (map-positions [tiled-map]
-    (for [x (range (width  tiled-map))
-          y (range (height tiled-map))]
-      [x y]))
-
-  (positions-with-property [tiled-map layer property-key]
-    (when (layer-index tiled-map layer)
-      (for [position (map-positions tiled-map)
-            :let [[x y] position
-                  value (property-value tiled-map layer position property-key)]
-            :when (not (#{:undefined :no-cell} value))]
-        [position value]))))
+(defn positions-with-property
+  "If the layer (keyword or layer object) does not exist returns nil.
+  Otherwise returns a sequence of [[x y] value] for all tiles who have property-key."
+  [tiled-map layer property-key]
+  (when (layer-index tiled-map layer)
+    (for [position (map-positions tiled-map)
+          :let [[x y] position
+                value (property-value tiled-map layer position property-key)]
+          :when (not (#{:undefined :no-cell} value))]
+      [position value])))
 
 (def
   ^{:doc "Memoized function.
